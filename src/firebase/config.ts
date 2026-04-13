81c5826464e727722c2e82830f9ea72b1b6514a3
Sumit Dhall <tradingsumitdhall@gmail.com>
2026-04-13 13:52:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2000326658-26edd",
+  "appId": "1:217028619138:web:de390323f637c1bc28fd9b",
+  "apiKey": "AIzaSyCq4pbpt31tNiBfvOP9mT_7pzVyJ9rOtq0",
+  "authDomain": "studio-2000326658-26edd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "217028619138"
+};
