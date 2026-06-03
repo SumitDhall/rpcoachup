5c589c18ddeffab0182be6e543632d50745c965c
Sumit Dhall <tradingsumitdhall@gmail.com>
2026-06-03 16:56:53 +0000

    increase the size a bit more

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -21,7 +21,7 @@ export function SiteFooter() {
           {/* Brand Section */}
           <div className="space-y-6 flex flex-col items-start col-span-1 sm:col-span-2 lg:col-span-1">
             <Link href="/" className="group block">
-              <div className="relative h-24 w-64 md:h-32 md:w-80 group-hover:scale-105 transition-transform duration-500">
+              <div className="relative h-32 w-80 md:h-48 md:w-[400px] group-hover:scale-105 transition-transform duration-500">
                 {brandLogo && (
                   <Image
                     src={brandLogo.imageUrl}
